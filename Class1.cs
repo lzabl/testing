832e83c2275b2f3ec3bf4b9afa7ce7d14332cf79
lzabl <laura.zabielaite@gmail.com>
2022-02-28 20:00:40 +0200

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTest1
+{
+    public class Class1
+    {
+        [Test]
+        public static void FirstTest()
+        {
+            int liekana = 5 % 2;
+            //Assert.AreEqual(0, liekana, "5 is not even");
+            Assert.IsTrue(0 == liekana, "5 is not even");
+        }
+
+        [Test]
+        public static void TestNow19()
+        {
+            DateTime currentTime = DateTime.Now;
+            Assert.AreEqual(19, currentTime.Hour, "Now is not 19");
+        }
+
+        
+
+    }
+}
